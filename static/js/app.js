82b48e5aadcff112769a2cb8aa56b872d29eb85a
Boyd Evans III <bbeiii@yahoo.com>
2021-06-14 10:47:58 -0600

    filter added, need to test

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,8 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var resetbutton = d3.select("#reset-btn");
-var inputField = d3.select("#datetime");
-var inputcity = d3.select("#city");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var populateTable = (dataInput) => {
@@ -20,3 +20,18 @@ var populateTable = (dataInput) => {
     });
 }
 
+//Populate the table with data
+populateTable(data);
+
+//Filter the data
+button.on("clilck",() => {
+    d3.event.preventDefault();
+    var inputDate = inputFieldDate.property("value").trim();
+    var inputCity = inputfieldCity.property("value").toLowerCase().trim();
+    var dateFilter = data.filter(data => data.datetime === inputDate);
+    var cityFilter = data.filter(data => data.city === inputCity);
+    var dataFilter = data.filter(data => data.datetime === inputData && data.city === inputcity);
+    console.log(dateFilter)
+    console.log(cityFilter)
+    console.log(dataFilter)
+})
