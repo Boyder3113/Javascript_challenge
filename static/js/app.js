9e6f6b24e02ef23124f3fe88cd3000493d6481e9
Boyd Evans III <bbeiii@yahoo.com>
2021-06-14 09:59:02 -0600

    adding starter files

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+//Create table rows using d3.select method
+
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var resetbutton = d3.select("#reset-btn");
+var inputField = d3.select("#datetime");
+var inputcity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
